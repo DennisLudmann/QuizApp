8c191a39e5659fe23268cad2cb40a2c94d9e92f1
DennisLudmann <dennisludmann@gmail.com>
2022-01-15 21:07:17 +0100

    color change, next-button

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 
-let questions= [{
+let questions = [{
 	"question": "What is the capital of Italy?",
 	"answer_1": "Madrid",
 	"answer_2": "Paris",
@@ -44,34 +44,74 @@ let questions= [{
 
 let currentQuest = 0;
 
-function init(){
+function init() {
 
-    document.getElementById('questions_counter').innerHTML = questions.length;
-    displayQuestion();
+	document.getElementById('questions_counter').innerHTML = questions.length;	// zeigt die Gesamtanzahl der Fragen an
+	displayQuestion();
 }
 
 
-function displayQuestion(){
-    let question = questions[currentQuest];
-    
-    document.getElementById('question_text').innerHTML = question['question'];
-    document.getElementById('answer_1').innerHTML = question['answer_1'];
-    document.getElementById('answer_2').innerHTML = question['answer_2'];
-    document.getElementById('answer_3').innerHTML = question['answer_3'];
-    document.getElementById('answer_4').innerHTML = question['answer_4'];
+function displayQuestion() {
+
+	if (currentQuest >= questions.length)
+		// END SCREAN
+} else {
+
+	let question = questions[currentQuest]; // questions ist oben das Json - currentQuestion ist global auf 0
+
+	document.getElementById('questions_counter_start').innerHTML = currentQuest + 1;
+	document.getElementById('question_text').innerHTML = question['question'];
+	document.getElementById('answer_1').innerHTML = question['answer_1'];
+	document.getElementById('answer_2').innerHTML = question['answer_2'];
+	document.getElementById('answer_3').innerHTML = question['answer_3'];
+	document.getElementById('answer_4').innerHTML = question['answer_4'];
+
+}
+}
+function answer(choice) {	// ein Parameter wird in die funktion übergeben 'answer_x'
+
+	let question = questions[currentQuest];					// currentQuestion ist am Anfang 0
+
+	console.log('selected answer is', choice)				// loggt den übergebenen Parameter
+
+	let selectedAnswerNumber = choice.slice(-1);			// gibt die letzte Ziffer vom übergebenen Parameter
+
+	let idOfRightAnswer = `answer_${question['right_answer']}`; // erkennt welche die Richtige Antwort ist
+
+	if (selectedAnswerNumber == question['right_answer']) {// wenn die letzte Ziffer vom übergebenen Parameter == der richtigen Antwort ist dann "if"
+		document.getElementById(choice).classList.add('bg-success');
+
+	} else {												// wenn sie nicht == ist dann "else"
+		document.getElementById(choice).classList.add('bg-danger');				// rot makiert weil antwort falsch ist
+		document.getElementById(idOfRightAnswer).classList.add('bg-success'); // makiert die Richtige Antwort, wenn eine falsche gewählt wurde
+	}
+
+	document.getElementById('next-button').disabled = false;
 
 }
 
-function answer(choice){
-    
+
+
+
+function nextQuestion() {
+	currentQuest++; 	// erhöht den wert von currentquest um 1
+	displayQuestion();	//	es wird auf das nächste elemnt im Json zugegriffen 
+
+	document.getElementById('next-button').disabled = true;
+
+	HideResults();
 
 }
 
+function HideResults() {
 
-
-
-function nextQuestion(){
-    let currentQuest = currentQuest + 1;
-    displayQuestion();
+	document.getElementById('answer_1').classList.remove('bg-danger');
+	document.getElementById('answer_1').classList.remove('bg-success');
+	document.getElementById('answer_2').classList.remove('bg-danger');
+	document.getElementById('answer_2').classList.remove('bg-success');
+	document.getElementById('answer_3').classList.remove('bg-danger');
+	document.getElementById('answer_3').classList.remove('bg-success');
+	document.getElementById('answer_4').classList.remove('bg-danger');
+	document.getElementById('answer_4').classList.remove('bg-success');
 
 }
